9de1c181963073b3914556677945b4c350c46517
Francois Reynaud <francois.reynaud@gmail.com>
2012-12-20 13:46:04 +0000

    mp

--- src/main/java/org/uiautomation/HelloWorld.java
+++ src/main/java/org/uiautomation/HelloWorld.java
@@ -1,8 +1,10 @@
 package org.uiautomation;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
 
@@ -11,38 +13,35 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class HelloWorld  extends HttpServlet {
+public class HelloWorld extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    resp.getWriter().print("test heroku!\n");
+    resp.getWriter().print("Hello Michael!\n");
 
-     try  {
-       Connection connection = Database.getConnection();
+    try {
+      Connection connection = Database.getConnection();
 
-       Statement stmt = connection.createStatement();
-       stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
-       stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
-       stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-       ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-       while (rs.next()) {
-         resp.getWriter().print("Read from DB: " + rs.getTimestamp("tick"));
-       }
-     } catch (Exception e){
-       resp.getWriter().print(e.getMessage());
-     }
-
-
-
+      Statement stmt = connection.createStatement();
+      stmt.executeUpdate("DROP TABLE IF EXISTS ticks");
+      stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+      while (rs.next()) {
+        resp.getWriter().print("Read from DB: " + rs.getTimestamp("tick"));
+      }
+    } catch (Exception e) {
+      resp.getWriter().print(e.getMessage());
+    }
   }
 
-  public static void main(String[] args) throws Exception{
+  public static void main(String[] args) throws Exception {
     Server server = new Server(Integer.valueOf(System.getenv("PORT")));
     ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
     context.setContextPath("/");
     server.setHandler(context);
-    context.addServlet(new ServletHolder(new HelloWorld()),"/*");
+    context.addServlet(new ServletHolder(new HelloWorld()), "/*");
     server.start();
     server.join();
   }
